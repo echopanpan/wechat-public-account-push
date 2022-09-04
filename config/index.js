55f2df00e21ed019142cae159356f0512d8e6984
echopanpan <112802889+echopanpan@users.noreply.github.com>
2022-09-04 18:13:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,48 +4,48 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "",
+  APP_ID: "mAoyrdmqASu1MEHe-XKDG-QZ7DLO_TlPYCwXPS-YBJI",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
+  APP_SECRET: "54e5d60c8662e61272055e9b13d32e61",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *    name: "小代同学0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "o4KJI67-iyxREljau9Hj1Pg46vOo",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "mAoyrdmqASu1MEHe-XKDG-QZ7DLO_TlPYCwXPS-YBJI",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://weibo.com/hot/weibo/102803"
    *   }, 
    * ]
    */
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "小代同学0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "o4KJI67-iyxREljau9Hj1Pg46vOo",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "mAoyrdmqASu1MEHe-XKDG-QZ7DLO_TlPYCwXPS-YBJI",
       // 所在省份
-      province: "广东",
+      province: "云南",
       // 所在城市
-      city: "肇庆",
+      city: "昆明",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://weibo.com/hot/weibo/102803"
     }, 
     {
-      name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
+      name: "小代同学", 
+      id: "o4KJI67-iyxREljau9Hj1Pg46vOo",
+      useTemplateId: "mAoyrdmqASu1MEHe-XKDG-QZ7DLO_TlPYCwXPS-YBJI",
+      province: "云南",
+      city: "昆明",
+      openUrl: "https://weibo.com/hot/weibo/102803"
     }, 
     {
       name: "老婆2",
@@ -78,7 +78,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "o4KJI65gzU_fqrxvMt0REB3Z1CjQ",
     }, 
   ],
     
@@ -89,9 +89,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "云南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "昆明",
 
   /** 重要节日相关 */
 
@@ -102,10 +102,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "小代同学", "year": "1998", "date": "10-02"},
+    {"type": "节日", "name": "认识", "year": "2004", "date": "09-01"},
+    {"type": "生日", "name": "小潘同学", "year": "1998", "date": "12-15"},
+    {"type": "节日", "name": "在一起", "year": "2022", "date": "02-22"},
   ],
 
   /**
@@ -141,14 +141,9 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
+    {"keyword": "encourage_oneself", contents: "久别重逢，佳期如梦，期待我们完整的朝夕♥"},
     // 这样配置的话，就会每次随机选一句话发送
-    {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
-    ]},
+   
   ],
 
   /** 每日一言 */
