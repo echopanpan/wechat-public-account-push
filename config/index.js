b760981dfc2a89a905146504b75ab5dd2586a21f
echopanpan <112802889+echopanpan@users.noreply.github.com>
2022-09-04 18:55:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,23 +1,156 @@
-{
-# 公众号配置
-# 公众号appId
-"app_id": "wx5e879ac2323552db",
-# 公众号appSecret
-"app_secret": "54e5d60c8662e61272055e9b13d32e61",
-# 模板消息id
-"template_id": "mAoyrdmqASu1MEHe-XKDG-QZ7DLO_TlPYCwXPS-YBJI",
-# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-"user": ["o4KJI67-iyxREljau9Hj1Pg46vOo","o4KJI65gzU_fqrxvMt0REB3Z1CjQ"],
+export const config = {
+  /**
+   * 公众号配置
+   */
 
-# 信息配置
-# 所在省份
-"province": "云南",
-# 所在城市
-"city": "昆明",
-# 生日1，修改name为对应需要显示的名字，如果生日为农历，在最前面加上r即可
-"birthday1": {"name": "小代", "birthday": "r1998-09-07"},
-# 生日2
-"birthday2": {"name": "小潘", "birthday": "1998-10-27"},
-# 在一起的日子，格式同上
-"love_date": "2022-02-22"
+  // 公众号APP_ID
+  APP_ID: "wx5e879ac2323552db",
+
+  // 公众号APP_SECRET
+  APP_SECRET: "54e5d60c8662e61272055e9b13d32e61",
+
+  /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
+   * [
+   *  {
+   *    // 想要发送的人的名字
+   *    name: "老婆0",
+   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    // 你想对他发送的模板消息的模板ID
+   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+   *    openUrl: "https://wangxinleo.cn"
+   *   }, 
+   * ]
+   */
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: "老婆0",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "WW-pTk2amnvEaMBGXWcLDCQmeplLUIOHh1EJ25iNn0E",
+      // 所在省份
+      province: "广东",
+      // 所在城市
+      city: "肇庆",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "小代同学", 
+      id: "o4KJI67-iyxREljau9Hj1Pg46vOo",
+      useTemplateId: "WW-pTk2amnvEaMBGXWcLDCQmeplLUIOHh1EJ25iNn0E",
+      province: "云南",
+      city: "昆明",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "老婆2",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "老婆3", 
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+  ],
+
+  /**
+   * 回调消息 相关，主要用来展示发送是否成功/失败的数据
+   */
+
+  // 回调消息模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+
+  // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
+  CALLBACK_USERS: [
+    {
+      // 一般都填自己
+      name: "自己",
+      // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "o4KJI65gzU_fqrxvMt0REB3Z1CjQ",
+    }, 
+  ],
+    
+  /**
+   * 信息配置
+   */
+
+  /** 天气相关 */
+
+  // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
+  PROVINCE: "广东",
+  // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
+  CITY: "惠州",
+
+  /** 重要节日相关 */
+
+  /**
+   * 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
+   * tpye必须填！ 只能 “生日” 和 “节日” 二选一!
+   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+   */
+  FESTIVALS: [
+    {"type": "生日", "name": "小代同学", "year": "1998", "date": "09-02"},
+    {"type": "节日", "name": "认识", "year": "2004", "date": "09-01"},
+    {"type": "生日", "name": "小潘同学", "year": "1998", "date": "12-15"},
+    {"type": "节日", "name": "久别重逢", "year": "2022", "date": "02-22"},
+  ],
+
+  /**
+   * 限制重要节日的展示条目, 需要填写数字; 
+   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
+   * 如果为0, 则默认展示全部
+   */
+  FESTIVALS_LIMIT: 4,
+
+  /** 日期相关 */
+
+  /** 你现在可以随心增加你认为的所有的需要纪念的日子啦！
+    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+    * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
+    * */
+  CUSTOMIZED_DATE_LIST: [
+    // 在一起的日子
+    {"keyword": "love_day", date: "2022-02-22"},
+    // 相识
+    {"keyword": "marry_day", date: "2004-09-01"},
+    // ... 
+  ],
+
+  /** 插槽 */
+
+  /** 你可以在这里写超多的你想显示的内容了！
+    * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
+    * 比如：keyword: "lover_prattle" ，在测试号中就是 {{ lover_prattle.DATA }} 
+    * */
+  SLOT_LIST: [
+    // 这样配置的话，就会每次发送这句话
+    {"keyword": "encourage_oneself", contents: "期待我们完整的朝夕"},
+    // 这样配置的话，就会每次随机选一句话发送
+    {"keyword": "lover_prattle", contents: [
+      "久别重逢，佳期如梦。",
+      "期待我们完整的朝夕。",
+      
+    ]},
+  ],
+
+  /** 每日一言 */
+
+  // 好文节选的内容类型
+  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+  LITERARY_PREFERENCE: ""
+
+
 }
